85525eb8bc179bacc2ab720cca2a3bcb48ba2926
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2023-10-30 15:17:32 -0300

    adjusts

---
+++ android/app/src/main/kotlin/com/example/repege/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.repege
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/core/utils/typedefs.dart
+++ lib/core/utils/typedefs.dart
@@ -1,10 +1,1 @@
-import 'package:dartz/dartz.dart';
-import 'package:repege/core/errors/failure.dart';
-
-typedef ResultStream<T> = Either<Failure, Stream<T>>;
-
-typedef ResultFuture<T> = Future<Either<Failure, T>>;
-
-typedef ResultVoid = Future<Either<Failure, void>>;
-
 typedef DataMap = Map<String, dynamic>;
